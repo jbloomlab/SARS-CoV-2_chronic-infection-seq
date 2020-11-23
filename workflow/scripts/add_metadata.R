366a5b301a60bc21ec3be350ad06785b71f1f7e4
Will Hannon <wwh22@uw.edu>
2020-11-22 19:32:55 -0800

    Fix the metadata script by reverting to old verision.

--- workflow/scripts/add_metadata.R
+++ workflow/scripts/add_metadata.R
@@ -30,10 +30,83 @@ empty = (info$size == 0)
 #
 # Add in any constrains on metadata here.
 #
-# Download an <-                            Effect  ==  "downstream_gene_variant"  ~ "Synonymous",
-Effect  ==  "stop_retained_variant"  ~ "Synonymous",
-Effect  ==  "stop_lost" ~ "Nonsense",
-Effect  ==  "start_lost"  ~ "Nonsense",
+# Download and format metedata. But first, check if the vcf is empty..
+if (!empty){
+  
+  metadata.df = read_csv(metadata.filepath) %>% rename(Accession = "Run")
+  
+  ## ==== Import and process the vcf -> txt files ==== ##
+  
+  # cols to split `ANN` vcf field into
+  cols_into = c("ALT_Allele", 
+                "Effect",
+                "Putative_Impact",
+                "Gene_Name",
+                "Gene_ID",
+                "Feature_Type",
+                "Feature_ID",
+                "Transcript_Biotype",
+                "Rank",
+                "HGVS.c",
+                "HGVS.p",
+                "cDNA_position",
+                "CDS_position", 
+                "Protein_position",
+                "Distance_to_feature",
+                "Errors")
+  
+  # extract metadata from filepath 
+  sample.info = strsplit(basename(vcf.filepath), "[.]")[[1]]
+  sample.name = sample.info[1]
+  sample.aligner = sample.info[2]
+  sample.caller = sample.info[3]
+  
+  # different extraction depending on columns in the original VCF file
+  if (sample.caller == "lofreq"){
+    # read in vcf table
+    sample.df = read_tsv(vcf.filepath) %>% 
+      # split into cols with regex
+      separate(col = ANN, sep = "\\|", remove = F, into = cols_into) %>% 
+      select(POS, REF,  ALT,
+             AF,  DP,
+             Effect, Gene_Name, AA_Change="HGVS.p") %>% 
+      # clean amino acid change column
+      mutate(AA_Change = gsub("^p\\.", "", AA_Change)) %>% 
+      # simplify the effect column
+      mutate(Effect = case_when(Effect  == "missense_variant" ~ "Missense",
+                                Effect  == "synonymous_variant" ~ "Synonymous",
+                                Effect  ==  "upstream_gene_variant"  ~ "Synonymous",
+                                Effect  ==  "downstream_gene_variant"  ~ "Synonymous",
+                                Effect  ==  "stop_retained_variant"  ~ "Synonymous",
+                                Effect  ==  "stop_lost" ~ "Nonsense",
+                                Effect  ==  "start_lost"  ~ "Nonsense",
+                                Effect  ==  "stop_gained" ~ "Nonsense")) %>% 
+      # add sample ID
+      mutate(Accession = sample.name) %>%
+      mutate(Caller = sample.caller) %>% 
+      mutate(Aligner = sample.aligner)
+    
+  } else if (sample.caller == "varscan"){
+    
+    # read in vcf table
+    sample.df = read_tsv(vcf.filepath) %>%
+      # split into cols with regex
+      separate(col = ANN, sep = "\\|", remove = F, into = cols_into) %>% 
+      select(POS, REF,  ALT,
+             AF="Sample1.FREQ",  DP="Sample1.DP",
+             Effect, Gene_Name, AA_Change="HGVS.p") %>% 
+      # clean amino acid change column
+      mutate(AA_Change = gsub("^p\\.", "", AA_Change)) %>% 
+      # clean allele freq column
+      mutate(AF = gsub("%", "", AF)) %>% 
+      # simplify the effect column
+      mutate(Effect = case_when(Effect  == "missense_variant" ~ "Missense",
+                                Effect  == "synonymous_variant" ~ "Synonymous",
+                                Effect  ==  "upstream_gene_variant"  ~ "Synonymous",
+                                Effect  ==  "downstream_gene_variant"  ~ "Synonymous",
+                                Effect  ==  "stop_retained_variant"  ~ "Synonymous",
+                                Effect  ==  "stop_lost" ~ "Nonsense",
+                                Effect  ==  "start_lost"  ~ "Nonsense",
                                 Effect  ==  "stop_gained" ~ "Nonsense")) %>% 
       # add sample ID
       mutate(Accession = sample.name) %>%
@@ -50,7 +123,7 @@ Effect  ==  "start_lost"  ~ "Nonsense",
   
   # Write out to a file
   write.csv(sample.df, file = snakemake@output[[1]], row.names=FALSE, sep="\t")
-
+  
 }else{
   # Write an empty output if file is empty
   cat(NULL,file=snakemake@output[[1]])
@@ -58,7 +131,6 @@ Effect  ==  "start_lost"  ~ "Nonsense",
 
 
 ## ==== END ==== ##
-
 
 
 
